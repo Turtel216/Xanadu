b5876555078f9f42e6d91e71f5c364b81c891969
Turtel216 <papadimitrios216@gmail.com>
2024-07-06 04:29:26 +0300

    Implemented token_tostring

--- interpreter/Types/Token.cpp
+++ interpreter/Types/Token.cpp
@@ -1,17 +1,53 @@
 #include "Token.h"
 #include "Literal.h"
+#include <map>
 #include <string>
 
 namespace xanadu::Tokens 
 {
 // Convert token enum to string
-std::string token_toString(TokenType type) noexcept 
+std::string& token_toString(const TokenType type) noexcept 
 {
-  switch (type) {
-    case LEFT_PAREN: return "(";
-    default: return "";
-      break;
-  }
+  static const std::map<TokenType, std::string> lookUpTable{
+      {TokenType::LEFT_PAREN, "LEFT_PAREN"},
+      {TokenType::RIGHT_PAREN, "RIGHT_PAREN"},
+      {TokenType::LEFT_BRACE, "LEFT_BRACE"},
+      {TokenType::RIGHT_BRACE, "RIGHT_BRACE"},
+      {TokenType::COMMA, "COMMA"},
+      {TokenType::DOT, "DOT"},
+      {TokenType::SEMICOLON, "SEMICOLON"},
+      {TokenType::SLASH, "SLASH"},
+      {TokenType::STAR, "STAR"},
+      {TokenType::BANG, "BANG"},
+      {TokenType::BANG_EQUAL, "BANG_EQUAL"},
+      {TokenType::EQUAL, "EQUAL"},
+      {TokenType::EQUAL_EQUAL, "EQUAL_EQUAL"},
+      {TokenType::GREATER, "GREATER"},
+      {TokenType::GREATER_EQUAL, "GREATER_EQUAL"},
+      {TokenType::LESS, "LESS"},
+      {TokenType::LESS_EQUAL, "LESS_EQUAL"},
+      {TokenType::MINUS, "MINUS"},
+      {TokenType::PLUS, "PLUS"},
+      {TokenType::IDENTIFIER, "IDENTIFIER"},
+      {TokenType::STRING, "STRING"},
+      {TokenType::NUMBER, "NUMBER"},
+      {TokenType::AND, "AND"},
+      {TokenType::CLASS, "CLASS"},
+      {TokenType::ELSE, "ELSE"},
+      {TokenType::FUN, "FUN"},
+      {TokenType::FOR, "FOR"},
+      {TokenType::IF, "IF"},
+      {TokenType::NIL, "NIL"},
+      {TokenType::OR, "OR"},
+      {TokenType::PRINT, "PRINT"},
+      {TokenType::RETURN, "RETURN"},
+      {TokenType::SUPER, "SUPER"},
+      {TokenType::THIS, "THIS"},
+      {TokenType::VAR, "VAR"},
+      {TokenType::WHILE, "WHILE"},
+      {TokenType::_EOF, "EOF"}};
+
+  return lookUpTable.find(type)->second;
 }
 
 // Getters
