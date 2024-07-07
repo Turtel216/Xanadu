d21d57d54c7391ae447a24edc972938af5407ef8
Turtel216 <papadimitrios216@gmail.com>
2024-07-07 13:36:18 +0300

    Update build files

---
+++ interpreter/build/CMakeFiles/xanadu_interpreter.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dimi/Documents/Dev/xanadu/interpreter/src/Scanner/Scanner.cpp" "CMakeFiles/xanadu_interpreter.dir/src/Scanner/Scanner.cpp.o" "gcc" "CMakeFiles/xanadu_interpreter.dir/src/Scanner/Scanner.cpp.o.d"
+  "/home/dimi/Documents/Dev/xanadu/interpreter/src/Types/Literal.cpp" "CMakeFiles/xanadu_interpreter.dir/src/Types/Literal.cpp.o" "gcc" "CMakeFiles/xanadu_interpreter.dir/src/Types/Literal.cpp.o.d"
+  "/home/dimi/Documents/Dev/xanadu/interpreter/src/Types/Token.cpp" "CMakeFiles/xanadu_interpreter.dir/src/Types/Token.cpp.o" "gcc" "CMakeFiles/xanadu_interpreter.dir/src/Types/Token.cpp.o.d"
+  "/home/dimi/Documents/Dev/xanadu/interpreter/src/Xanadu.cpp" "CMakeFiles/xanadu_interpreter.dir/src/Xanadu.cpp.o" "gcc" "CMakeFiles/xanadu_interpreter.dir/src/Xanadu.cpp.o.d"
+  "/home/dimi/Documents/Dev/xanadu/interpreter/src/main.cpp" "CMakeFiles/xanadu_interpreter.dir/src/main.cpp.o" "gcc" "CMakeFiles/xanadu_interpreter.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ interpreter/build/CMakeFiles/xanadu_interpreter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for xanadu_interpreter.
+# This may be replaced when dependencies are built.
