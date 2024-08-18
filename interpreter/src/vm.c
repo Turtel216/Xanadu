f0235623ec797f341d3ba286785f5197ceac19f6
Turtel216 <papadimitrios216@gmail.com>
2024-08-18 18:09:01 +0300

    Added arithmetic operators

--- interpreter/src/vm.c
+++ interpreter/src/vm.c
@@ -1,3 +1,4 @@
+#include "chunk.h"
 #include "common.h"
 #include "debug.h"
 #include <stdio.h>
@@ -23,6 +24,12 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)             \
+	do {                      \
+		double b = pop(); \
+		double a = pop(); \
+		push(a op b);     \
+	} while (false)
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -45,6 +52,21 @@ static InterpretResult run()
 			push(constant);
 			break;
 		}
+		case OP_ADD:
+			BINARY_OP(+);
+			break;
+		case OP_SUBTRACT:
+			BINARY_OP(-);
+			break;
+		case OP_MULTIPLY:
+			BINARY_OP(*);
+			break;
+		case OP_DIVIDE:
+			BINARY_OP(/);
+			break;
+		case OP_NEGATE:
+			push(-pop());
+			break;
 		case OP_RETURN: {
 			print_value(pop());
 			printf("\n");
@@ -55,6 +77,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
