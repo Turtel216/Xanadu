1a5082747d44e6a334059eef9aa3f376d65061bc
Turtel216 <papadimitrios216@gmail.com>
2024-09-18 13:13:05 +0300

    Updated object module documentation

--- interpreter/src/object.c
+++ interpreter/src/object.c
@@ -1,6 +1,6 @@
 // copyright 2024 dimitrios papakonstantinou. all rights reserved.
-// use of this source code is governed by a MIT
-// license that can be found in the license file.
+// use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
 
 #define ALLOCATE_OBJ(type, objectType) \
 	(type *)allocate_object(sizeof(type), objectType)
@@ -15,7 +15,10 @@
 #include "vm.h"
 #include "lookup_table.h"
 
-// Print function types to iostream
+// Print a function object to the output stream
+// Parameters:
+//   function - The function object to print
+// If the function does not have a name, "<script>" is printed.
 static void print_function(ObjFunction *function)
 {
 	if (function->name == NULL) {
@@ -25,152 +28,200 @@ static void print_function(ObjFunction *function)
 	printf("<fn %s>", function->name->chars);
 }
 
-// Print object types to iostream
+// Print the representation of an object to the output stream
+// Parameters:
+//   value - The value representing the object to print
 void print_object(Value value)
 {
 	switch (OBJ_TYPE(value)) {
 	case OBJ_BOUND_METHOD:
+		// Print bound method by delegating to function printer
 		print_function(AS_BOUND_METHOD(value)->method->function);
 		break;
 	case OBJ_INSTANCE:
+		// Print class name of the instance
 		printf("%s instance", AS_INSTANCE(value)->class_->name->chars);
 		break;
 	case OBJ_CLASS:
+		// Print class name
 		printf("%s", AS_CLASS(value)->name->chars);
 		break;
 	case OBJ_STRING:
+		// Print string contents
 		printf("%s", AS_CSTRING(value));
 		break;
 	case OBJ_FUNCTION:
+		// Print function object
 		print_function(AS_FUNCTION(value));
 		break;
 	case OBJ_NATIVE:
+		// Print native function placeholder
 		printf("<native fn>");
 		break;
 	case OBJ_CLOSURE:
+		// Print closure function
 		print_function(AS_CLOSURE(value)->function);
 		break;
 	case OBJ_UPVALUE:
+		// Print upvalue placeholder
 		printf("upvalue");
 		break;
 	}
 }
 
-// Allocate new Object with specified type and size
+// Allocate a new object of a specified type and size
+// Parameters:
+//   size - The size of the object to allocate
+//   type - The type of the object being allocated
+// Returns:
+//   A pointer to the newly allocated object
 static Obj *allocate_object(size_t size, ObjType type)
 {
+	// Allocate memory for the new object
 	Obj *object = (Obj *)reallocate(NULL, 0, size);
 	object->type = type;
-	object->next = vm.objects;
-	object->is_marked = false;
-	vm.objects = object;
+	object->next = vm.objects; // Link new object into the list
+	object->is_marked = false; // Initial state: not marked for GC
+	vm.objects = object; // Update the head of the list
 
 #ifdef DEBUG_LOG_GC
+	// Log the allocation if debugging GC
 	printf("%p allocate %zu for %d\n", (void *)object, size, type);
 #endif
 	return object;
 }
 
-// Calcute hash valye of string
+// Compute a hash value for a given string
+// Parameters:
+//   key    - The string to hash
+//   length - The length of the string
+// Returns:
+//   The computed hash value
 static uint32_t hash_string(const char *key, int length)
 {
-	uint32_t hash = 2166136261u;
+	uint32_t hash = 2166136261u; // FNV-1a initial hash value
 	for (int i = 0; i < length; i++) {
-		hash ^= (uint8_t)key[i];
-		hash *= 16777619;
+		hash ^= (uint8_t)key[i]; // XOR the byte with the hash
+		hash *= 16777619; // Multiply by the FNV-1a prime
 	}
 	return hash;
 }
 
-// Alocate new ObjString and return said object
+// Allocate a new ObjString and initialize it
+// Parameters:
+//   chars - The character array for the string
+//   length - The length of the string
+//   hash - The precomputed hash value of the string
+// Returns:
+//   A pointer to the newly allocated ObjString
 static ObjString *allocate_string(char *chars, int length, uint32_t hash)
 {
-	// Create new String object
+	// Allocate and initialize a new string object
 	ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
 	string->length = length;
 	string->chars = chars;
 	string->hash = hash;
 
+	// Manage the object in the GC and insert into the hash table
 	push(OBJ_VAL(string));
-
-	// Insert object into hash table
 	insert_into_table(&vm.strings, string, NIL_VAL);
-
 	pop();
 
 	return string;
 }
 
-// Copy string to new ObjString and return new object
+// Copy a string into a new ObjString and return it
+// Parameters:
+//   chars - The character array of the string to copy
+//   length - The length of the string
+// Returns:
+//   A pointer to the new ObjString
 ObjString *copy_string(const char *chars, int length)
 {
-	// Calculate hash value
+	// Compute hash value of the string
 	uint32_t hash = hash_string(chars, length);
+	// Check if the string is already interned
 	ObjString *interned =
 		table_find_string(&vm.strings, chars, length, hash);
 
-	// Notify user since string not found on the hash table
 	if (interned != NULL)
-		return interned;
+		return interned; // Return existing string if found
 
-	// Alocate and copy memory
+	// Allocate memory for a new string and copy the contents
 	char *heap_chars = ALLOCATE(char, length + 1);
 	memcpy(heap_chars, chars, length);
-	heap_chars[length] = '\0';
-	// Allocate new string and return String object
+	heap_chars[length] = '\0'; // Null-terminate the string
+
+	// Allocate and return the new string object
 	return allocate_string(heap_chars, length, hash);
 }
 
-// Create a string object
+// Create a new ObjString by taking ownership of an existing character array
+// Parameters:
+//   chars - The character array for the string (will be freed if already interned)
+//   length - The length of the string
+// Returns:
+//   A pointer to the new ObjString
 ObjString *take_string(char *chars, int length)
 {
-	// Calculate hash value
+	// Compute hash value of the string
 	uint32_t hash = hash_string(chars, length);
 
-	// Insert new string into hash table
+	// Check if the string is already interned
 	ObjString *interned =
 		table_find_string(&vm.strings, chars, length, hash);
 	if (interned != NULL) {
+		// Free the old memory and return the interned string
 		FREE_ARRAY(char, chars, length + 1);
 		return interned;
 	}
 
-	// Allocate and return new String object
+	// Allocate and return the new string object
 	return allocate_string(chars, length, hash);
 }
 
-// Create a function object
+// Create a new ObjFunction object
+// Returns:
+//   A pointer to the newly created ObjFunction
 ObjFunction *new_function(void)
 {
-	// Create new function object
+	// Allocate and initialize a new function object
 	ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
-	function->arity = 0;
-	function->upvalueCount = 0;
-	function->name = NULL;
+	function->arity = 0; // Default arity is 0
+	function->upvalueCount = 0; // No upvalues initially
+	function->name = NULL; // Function name is not set
 
-	// Initialize byte code data and return object
+	// Initialize the bytecode chunk for the function
 	init_chunk(&function->chunk);
 	return function;
 }
 
-// Create a native object
+// Create a new ObjNative object for a native function
+// Parameters:
+//   function - The native function pointer
+// Returns:
+//   A pointer to the newly created ObjNative
 ObjNative *new_native(NativeFn function)
 {
 	ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
-	native->function = function;
+	native->function = function; // Set the native function pointer
 	return native;
 }
 
-// Create a closure object
+// Create a new ObjClosure object
+// Parameters:
+//   function - The function that this closure wraps
+// Returns:
+//   A pointer to the newly created ObjClosure
 ObjClosure *new_closure(ObjFunction *function)
 {
-	// Create Upvalue object and initialize upvalues
+	// Allocate and initialize upvalues
 	ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue *, function->upvalueCount);
 	for (int i = 0; i < function->upvalueCount; i++) {
-		upvalues[i] = NULL;
+		upvalues[i] = NULL; // Upvalues are initially uninitialized
 	}
 
-	// Create closure object
+	// Create and initialize the closure object
 	ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
 	closure->function = function;
 	closure->upvalues = upvalues;
@@ -178,39 +229,57 @@ ObjClosure *new_closure(ObjFunction *function)
 	return closure;
 }
 
-// Create a upvalue obejct
+// Create a new ObjUpvalue object
+// Parameters:
+//   slot - The location of the upvalue on the stack
+// Returns:
+//   A pointer to the newly created ObjUpvalue
 ObjUpvalue *new_upvalue(Value *slot)
 {
 	ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
-	upvalue->location = slot;
-	upvalue->next = NULL;
-	upvalue->closed = NIL_VAL;
+	upvalue->location = slot; // Set the location of the upvalue
+	upvalue->next = NULL; // No next upvalue initially
+	upvalue->closed = NIL_VAL; // Initial closed value is NIL
 	return upvalue;
 }
 
-// Create a Class Object
+// Create a new ObjClass object
+// Parameters:
+//   name - The name of the class
+// Returns:
+//   A pointer to the newly created ObjClass
 ObjClass *new_class(ObjString *name)
 {
 	ObjClass *klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
-	klass->name = name;
-	init_table(&klass->methods);
+	klass->name = name; // Set the class name
+	init_table(&klass->methods); // Initialize the class methods table
 
 	return klass;
 }
 
+// Create a new ObjInstance object
+// Parameters:
+//   class_ - The class to which this instance belongs
+// Returns:
+//   A pointer to the newly created ObjInstance
 ObjInstance *new_instance(ObjClass *class_)
 {
 	ObjInstance *instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
-	instance->class_ = class_;
-	init_table(&instance->fields);
+	instance->class_ = class_; // Set the class of the instance
+	init_table(&instance->fields); // Initialize the instance fields table
 	return instance;
 }
 
-// Create a object instance Object
+// Create a new ObjBoundMethod object
+// Parameters:
+//   receiver - The instance that the method is bound to
+//   method - The method (closure) being bound
+// Returns:
+//   A pointer to the newly created ObjBoundMethod
 ObjBoundMethod *new_bound_method(Value receiver, ObjClosure *method)
 {
 	ObjBoundMethod *bound = ALLOCATE_OBJ(ObjBoundMethod, OBJ_BOUND_METHOD);
-	bound->receiver = receiver;
-	bound->method = method;
+	bound->receiver = receiver; // Set the bound instance
+	bound->method = method; // Set the bound method (closure)
 	return bound;
 }
--- interpreter/src/object.h
+++ interpreter/src/object.h
@@ -1,6 +1,6 @@
 // copyright 2024 dimitrios papakonstantinou. all rights reserved.
-// use of this source code is governed by a MIT
-// license that can be found in the license file.
+// use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
 
 #ifndef xanadu_object_h
 #define xanadu_object_h
@@ -10,145 +10,216 @@
 #include "chunk.h"
 #include "lookup_table.h"
 
-// Macro for getting the type of an object
+// Macros for type-checking and type-casting objects in the VM
+
+// Retrieve the type of the object from a Value
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
-// Macro for checking if an object is a string object
+
+// Check if a Value is a string object
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
-// Macro for checking if an object is a native object
+
+// Check if a Value is a native object (C function)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
-// Macro for checking if an object is a closure object
+
+// Check if a Value is a closure object (function with upvalues)
 #define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
-// Macro for converting a Value type to a Object String
+
+// Convert a Value to an ObjString object
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
-// Macro for converting a value to type to a cstring
+
+// Convert a Value to a C-string (char array) from an ObjString
 #define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
-// Macro for converting a value type to a function object
+
+// Convert a Value to an ObjFunction object
 #define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
-// Macro for converting a value type to a native object
+
+// Convert a Value to an ObjNative object
 #define AS_NATIVE(value) (((ObjNative *)AS_OBJ(value))->function)
-// Macro for converting a value type to a closure object
+
+// Convert a Value to an ObjClosure object
 #define AS_CLOSURE(value) ((ObjClosure *)AS_OBJ(value))
-// Macro for checking if an object is a function object
+
+// Check if a Value is a function object
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
-// Macro for checking if an object is a xanadu object
+
+// Check if a Value is a class object
 #define IS_CLASS(value) isObjType(value, OBJ_CLASS)
-// Macro for converting a value type to a xanadu object
+
+// Convert a Value to an ObjClass object
 #define AS_CLASS(value) ((ObjClass *)AS_OBJ(value))
-// Macro for checking if an object is a xanadu object instance
+
+// Check if a Value is an instance of a class object
 #define IS_INSTANCE(value) isObjType(value, OBJ_INSTANCE)
-// Macro for converting a value type to a xanadu object instance
+
+// Convert a Value to an ObjInstance object
 #define AS_INSTANCE(value) ((ObjInstance *)AS_OBJ(value))
-// Macro for checking if an object is a xanadu bound method
+
+// Check if a Value is a bound method object
 #define IS_BOUND_METHOD(value) isObjType(value, OBJ_BOUND_METHOD)
-// Macro for converting a value type to a xanadu bound method
+
+// Convert a Value to an ObjBoundMethod object
 #define AS_BOUND_METHOD(value) ((ObjBoundMethod *)AS_OBJ(value))
 
-// Enum containing the types of objects
+// Enum for different object types in the VM
 typedef enum {
-	OBJ_STRING,
-	OBJ_FUNCTION,
-	OBJ_NATIVE,
-	OBJ_CLOSURE,
-	OBJ_UPVALUE,
-	OBJ_CLASS,
-	OBJ_INSTANCE,
-	OBJ_BOUND_METHOD,
+	OBJ_STRING, // String object
+	OBJ_FUNCTION, // Function object
+	OBJ_NATIVE, // Native (C function) object
+	OBJ_CLOSURE, // Closure object
+	OBJ_UPVALUE, // Upvalue object
+	OBJ_CLASS, // Class object
+	OBJ_INSTANCE, // Instance of a class
+	OBJ_BOUND_METHOD, // Bound method object
 } ObjType;
 
-// Object for the Xanadu VM
+// Base structure for all objects in the Xanadu VM
 struct Obj {
-	ObjType type; // Type of object
-	struct Obj *next; // Pointer to next object in object list
-	bool is_marked; // Track if object is marked for gc
+	ObjType type; // Type of the object
+	struct Obj *next; // Pointer to the next object in the list
+	bool is_marked; // Flag indicating if the object is marked for garbage collection
 };
 
-// Object wrapper for function object
+// Object representing a function in the VM
 typedef struct {
-	Obj obj; // Object type
-	int arity; // Number of function arguments
-	Chunk chunk; // Bytecode info
-	ObjString *name; // String info
-	int upvalueCount; // Number of upvalues
+	Obj obj; // Base object structure
+	int arity; // Number of arguments the function takes
+	Chunk chunk; // Bytecode chunk representing the function's code
+	ObjString *name; // Name of the function
+	int upvalueCount; // Number of upvalues captured by the function
 } ObjFunction;
 
-// Function pointer for Native Functions
+// Type for native functions (C functions callable from the VM)
 typedef Value (*NativeFn)(int argCount, Value *args);
 
-// Object wrapper for Native Objects
+// Object representing a native function (C function) in the VM
 typedef struct {
-	Obj obj; // Xanadu VM object
-	NativeFn function; // Native function
+	Obj obj; // Base object structure
+	NativeFn function; // Pointer to the C function
 } ObjNative;
 
-// Object wrapper for String object
+// Object representing a string in the VM
 struct ObjString {
-	Obj obj; // Object data
-	int length; // String length
-	char *chars; // String character array
-	uint32_t hash; // Hash value
+	Obj obj; // Base object structure
+	int length; // Length of the string
+	char *chars; // Character array for the string
+	uint32_t hash; // Hash value for the string
 };
 
-// Object wrapper for Upvalue objects
+// Object representing an upvalue (closed-over variable) in the VM
 typedef struct ObjUpvalue {
-	Obj obj; // Xanadu VM Object
-	Value *location; // Location of the upvalue on the stack
-	struct ObjUpvalue *next; // Pointer to next node in upvalue linked list
-	Value closed; // Hold closed upvalue value
+	Obj obj; // Base object structure
+	Value *location; // Pointer to the stack location of the upvalue
+	struct ObjUpvalue *next; // Pointer to the next upvalue in the list
+	Value closed; // The value of the closed-over variable
 } ObjUpvalue;
 
-// Object wrapper for Closure objects
+// Object representing a closure (function with captured upvalues) in the VM
 typedef struct {
-	Obj obj; // Xanadu VM Object
-	ObjFunction *function; // Array of functions on the call stack
-	ObjUpvalue **upvalues; // Array of upvalues
-	int upvalueCount; //Number of upvalues associeted with the closure object
+	Obj obj; // Base object structure
+	ObjFunction *function; // The function that this closure wraps
+	ObjUpvalue **upvalues; // Array of upvalues captured by the closure
+	int upvalueCount; // Number of upvalues associated with the closure
 } ObjClosure;
 
-// Object wrapper for Xanadu Objects
+// Object representing a class in the VM
 typedef struct {
-	Obj obj;
-	ObjString *name;
-	Table methods; // Class methods
+	Obj obj; // Base object structure
+	ObjString *name; // Name of the class
+	Table methods; // Table of methods defined for the class
 } ObjClass;
 
-// Object wrapper for Xanadu Object Instances
+// Object representing an instance of a class in the VM
 typedef struct {
-	Obj obj;
-	ObjClass *class_;
-	Table fields;
+	Obj obj; // Base object structure
+	ObjClass *class_; // The class that this instance belongs to
+	Table fields; // Table of fields for this instance
 } ObjInstance;
 
-// Object wrapper for Xanadu bound method
+// Object representing a bound method (method bound to an instance) in the VM
 typedef struct {
-	Obj obj;
-	Value receiver;
-	ObjClosure *method;
+	Obj obj; // Base object structure
+	Value receiver; // The instance to which the method is bound
+	ObjClosure *method; // The method (closure) being bound
 } ObjBoundMethod;
 
-// Returns true value is of the given object type
+// Check if a value is of a specific object type
+// Parameters:
+//   value - The value to check
+//   type  - The type to compare against
+// Returns:
+//   true if the value is of the specified type; false otherwise
 static inline bool isObjType(Value value, ObjType type)
 {
 	return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
-// Copy string to new ObjString and return new object
+// Create a new ObjString by copying the provided string
+// Parameters:
+//   chars  - The string to copy
+//   length - Length of the string
+// Returns:
+//   A pointer to the newly created ObjString
 ObjString *copy_string(const char *chars, int length);
-// Print object types to iostream
+
+// Print the type and value of an object to standard output
+// Parameters:
+//   value - The value to print
 void print_object(Value value);
-// Create a string object
+
+// Create a new ObjString by taking ownership of the provided string
+// Parameters:
+//   chars  - The string to take ownership of
+//   length - Length of the string
+// Returns:
+//   A pointer to the newly created ObjString
 ObjString *take_string(char *chars, int length);
-// Create a function object
+
+// Create a new ObjFunction object
+// Returns:
+//   A pointer to the newly created ObjFunction
 ObjFunction *new_function(void);
-// Create a native object
+
+// Create a new ObjNative object
+// Parameters:
+//   function - The native function to wrap
+// Returns:
+//   A pointer to the newly created ObjNative
 ObjNative *new_native(NativeFn function);
-// Create a closure object
+
+// Create a new ObjClosure object
+// Parameters:
+//   function - The function that the closure wraps
+// Returns:
+//   A pointer to the newly created ObjClosure
 ObjClosure *new_closure(ObjFunction *function);
-// Create a upvalue obejct
+
+// Create a new ObjUpvalue object
+// Parameters:
+//   slot - Pointer to the stack location of the upvalue
+// Returns:
+//   A pointer to the newly created ObjUpvalue
 ObjUpvalue *new_upvalue(Value *slot);
-// Create a Class Object
+
+// Create a new ObjClass object
+// Parameters:
+//   name - The name of the class
+// Returns:
+//   A pointer to the newly created ObjClass
 ObjClass *new_class(ObjString *name);
-// Create a object instance Object
+
+// Create a new ObjInstance object
+// Parameters:
+//   class_ - The class that the instance belongs to
+// Returns:
+//   A pointer to the newly created ObjInstance
 ObjInstance *new_instance(ObjClass *class_);
-// Create a object instance Object
+
+// Create a new ObjBoundMethod object
+// Parameters:
+//   receiver - The instance to which the method is bound
+//   method   - The method (closure) being bound
+// Returns:
+//   A pointer to the newly created ObjBoundMethod
 ObjBoundMethod *new_bound_method(Value receiver, ObjClosure *method);
+
 #endif
